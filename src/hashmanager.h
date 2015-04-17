7b968694c5f721a48cce01eda35b3cfcbeb07cd5
Cassio <cassio.terceiro@gmail.com>
2015-04-17 02:23:57 -0300

    namespace lhash created

--- src/hashmanager.h
+++ src/hashmanager.h
@@ -1,126 +1,147 @@
 #include <cstdio>
-#include <cassert>
 #include "page.h"
 #include <unistd.h>
 
-class hash_manager {
-	private :
-		std::string file_name;
-		FILE* file;
-		char buffer[page::page_size];
-		int original_size;
-		int n_buckets;
-		int n_pages;
-		int level;
-		int next;
-		//Take f modulo n*2^level to be the hash function
-		std::function<int (int)> func;
-		int hash0(int);
-		int hash1(int);
-		void load_page_to_buffer(int);
-		void write_page_to_buffer(page*);
-		void save_buffer_to_file(int);
-		page* page_from_buffer();
-		inline fheader_page* load_fheader_page();
-		inline pointer_page* load_pointer_page();
-		inline content_page* load_content_page();
-		void buid_from_file();
-		void build_new();
-	public :
-		hash_manager(std::string, std::function<int (int)>);
-		~hash_manager();
-		void insert_data(int,int);
-		void remove_data(int);
-		int find_data(int);
-};
+namespace lhash {
 
-hash_manager::hash_manager(std::string fname, std::function<int (int)> f = [](int x){return x;}) : file_name(fname), func(f) {
-	//if file exists
-	if (access(file_name.c_String(),F_OK) != -1) build_from_file();
-	else build_new();
-}
-
-hash_manager::~hash_manager() {
-	fclose(file);
-}
-void hash_manager::hash_manager build_new() {
-	file = fopen(file_name.c_str(),"w+");
-	//TODO
-}
-
-void hash_manager::buid_from_file() {
-	file = fopen(file_name.c_str(),"r+");
-	load_page_to_buffer(0);
-	fheader_page* header_page = dynamic_cast<fheader_page*>(page_from_buffer());
-	original_size = header_page->get_orig_size();
-	n_buckets = header_page->get_n_buckets();
-	n_pages = header_page->get_n_pages();
-	level = header_page->get_level();
-	next = header_page->get_next();
-	delete header_page;
-}
-
-void hash_manager::insert_data(int search_key, int rid) {
-	//TODO
-}
-
-void hash_manager::remove_data(int search_key) {
-	//TODO
-}
-
-int hash_manager::find_data(int search_key) {
-	//TODO
-	return 0;
-}
-
-int hash_manager::hash0 (int search_key) {
-	//TODO
-	return 0;
-}
-
-int hash_manager::hash1 (int search_key) {
-	//TODO
-	return 0;
-}
-
-void hash_manager::load_page_to_buffer(int byte_pos) {
-	fseek(file,byte_pos,SEEK_SET);
-	fread(buffer,page::page_size,file);
-}
-
-void hash_manager::save_buffer_to_file(int byte_pos) {
-	fseek(file,byte_pos,SEEK_SET);
-	fwrite(buffer,page::page_file,1,file);
-}
-
-//load page info into buffer, deletes page.
-void hash_manager::write_page_to_buffer(page* p) {
-	//TODO
-}
-
-inline fheader_page* hash_manager::load_fheader_page() {
-	//TODO
-	return nullptr;
-}
-
-inline pointer_page* hash_manager::load_pointer_page() {
-	pointer_page* new_page = new pointer_page();
-}
-
-inline content_page* hash_manager::load_content_page() {
-	//TODO
-	return nullptr;
-}
-
-//needs to allocate page and load its info from the buffer
-page* hash_manager::page_from_buffer() {
-	switch (buffer[0]) {
-		case page::fheader_page :
-			return static_cast<page*>(load_header_page());
-		case page::pointer_page :
-			return static_cast<page*>(load_pointer_page());
-		case page::content_page :
-			return static_cast<page*>(load_content_page());
+	int chars_to_int (char* c) {
+		return (c[0] << 24)+(c[1] << 16)+(c[2] << 8)+c[3];
 	}
-	return nullptr;
+
+	class hash_manager {
+		private :
+			std::string file_name;
+			FILE* file;
+			char buffer[page::page_size];
+			int original_size;
+			int n_buckets;
+			int n_pages;
+			int level;
+			int next;
+			//Take f modulo n*2^level to be the hash function
+			std::function<int (int)> func;
+			int hash0(int);
+			int hash1(int);
+			void load_page_to_buffer(int);
+			void write_page_to_buffer(page*);
+			void save_buffer_to_file(int);
+			page* page_from_buffer();
+			inline fheader_page* load_fheader_page();
+			inline pointer_page* load_pointer_page();
+			inline content_page* load_content_page();
+			void buid_from_file();
+			void build_new();
+		public :
+			hash_manager(std::string, std::function<int (int)>);
+			~hash_manager();
+			void insert_data(int,int);
+			void remove_data(int);
+			int find_data(int);
+	};
+
+	hash_manager::hash_manager(std::string fname, std::function<int (int)> f = [](int x){return x;}) : file_name(fname), func(f) {
+		//if file exists
+		if (access(file_name.c_String(),F_OK) != -1) build_from_file();
+		else build_new();
+	}
+
+	hash_manager::~hash_manager() {
+		fclose(file);
+	}
+	void hash_manager::hash_manager build_new() {
+		file = fopen(file_name.c_str(),"w+");
+		//TODO
+	}
+
+	void hash_manager::buid_from_file() {
+		file = fopen(file_name.c_str(),"r+");
+		load_page_to_buffer(0);
+		fheader_page* header_page = dynamic_cast<fheader_page*>(page_from_buffer());
+		original_size = header_page->get_orig_size();
+		n_buckets = header_page->get_n_buckets();
+		n_pages = header_page->get_n_pages();
+		level = header_page->get_level();
+		next = header_page->get_next();
+		delete header_page;
+	}
+
+	void hash_manager::insert_data(int search_key, int rid) {
+		//TODO
+	}
+
+	void hash_manager::remove_data(int search_key) {
+		//TODO
+	}
+
+	int hash_manager::find_data(int search_key) {
+		//TODO
+		return 0;
+	}
+
+	int hash_manager::hash0 (int search_key) {
+		//TODO
+		return 0;
+	}
+
+	int hash_manager::hash1 (int search_key) {
+		//TODO
+		return 0;
+	}
+
+	void hash_manager::load_page_to_buffer(int byte_pos) {
+		fseek(file,byte_pos,SEEK_SET);
+		fread(buffer,page::page_size,file);
+	}
+
+	void hash_manager::save_buffer_to_file(int byte_pos) {
+		fseek(file,byte_pos,SEEK_SET);
+		fwrite(buffer,page::page_file,1,file);
+	}
+
+	//load page info into buffer, deletes page.
+	void hash_manager::write_page_to_buffer(page* p) {
+		//TODO
+	}
+
+	inline fheader_page* hash_manager::load_fheader_page() {
+		fheader_page* new_page = new fheader_page();
+		new_page->set_orig_size(chars_to_int(&buffer[12]));
+		new_page->set_n_buckets(chars_to_int(&buffer[16]));
+		new_page->set_n_pages(chars_to_int(&buffer[20]));
+		new_page->set_level(chars_to_int(&buffer[24]));
+		new_page->set_next(chars_to_int(&buffer[28]));
+		return new_page;
+	}
+
+	inline pointer_page* hash_manager::load_pointer_page() {
+		pointer_page* new_page = new pointer_page();
+		std::bitset<n_pointers> ptr_bitmap(chars_to_int(&buffer[12]));
+		int j = 0;
+		for (int i=16 ; i<page_size-3 ; i+=4) {
+			if (ptr_bitmap[j++]) new_page->add_ptr(chars_to_int(&buffer[i]))
+		}
+	}
+
+	inline content_page* hash_manager::load_content_page() {
+		content_page* new_page = new content_page();
+		std::bitset<n_data> data_bitmap(chars_to_int(&buffer[12]);
+		int j = 0;
+		for (int i=6 ; i<page_size-7 ; i+=8) {
+			if (data_bitmap[j++]) new_page->add_data(std::make_pair(chars_to_int(&buffer[i]),chars_to_int(&buffer[i+4])));
+		}
+		return new_page;
+	}
+
+	//needs to allocate page and load its info from the buffer
+	page* hash_manager::page_from_buffer() {
+		switch (buffer[0]) {
+			case page::fheader_page :
+				return static_cast<page*>(load_header_page());
+			case page::pointer_page :
+				return static_cast<page*>(load_pointer_page());
+			case page::content_page :
+				return static_cast<page*>(load_content_page());
+		}
+		return nullptr;
+	}
 }
--- src/page.h
+++ src/page.h
@@ -2,145 +2,142 @@
 #include <bitset>
 #include <iostream>
 
+namespace lhash {
+	//base class which has the page header already defined, since it is common to all page types
+	class page {
+		protected :
+			int parent_pointer;
+			int disc_writes;
 
-//base class which has the page header already defined, since it is common to all page types
-class page {
-	protected :
-		int parent_pointer;
-		int disc_writes;
+		public :
+			const char type;
+			page (char t) : type(t), parent_pointer(-1), disc_writes(0) {};
+			page (char t, int parent_ptr, int dw) : type(t), parent_pointer(parent_ptr), disc_writes(dw) {}
+			virtual ~page() {}
+			inline void set_parent_ptr (int);
+			inline int get_parent_ptr ();
+			inline int incr_disc_writes ();
+	};
 
-	public :
-		const char type;
-		static const char fheader_page = 0;
-		static const char pointer_page = 1;
-		static const char content_page = 2;
-		static const int page_size = 128;
-		page (char t) : type(t), parent_pointer(-1), disc_writes(0) {};
-		page (char t, int parent_ptr, int dw) : type(t), parent_pointer(parent_ptr), disc_writes(dw) {}
-		virtual ~page() {}
-		inline void set_parent_ptr (int);
-		inline int get_parent_ptr ();
-		inline int incr_disc_writes ();
-};
+	inline void page::set_parent_ptr(int ptr) { parent_pointer = ptr; }
+	inline int page::get_parent_ptr() { return parent_pointer; }
+	inline int page::incr_disc_writes() { return ++disc_writes; }
 
-inline void page::set_parent_ptr(int ptr) { parent_pointer = ptr; }
-inline int page::get_parent_ptr() { return parent_pointer; }
-inline int page::incr_disc_writes() { return ++disc_writes; }
+	//-----------end page class-----------------
 
-//-----------end page class-----------------
+	//file header page. Contains the info about the hash table in the file
+	class fheader_page : public page {
+		private :
+			int original_size;
+			int n_buckets;
+			int n_pages;
+			int level;
+			int next;
 
-//file header page. Contains the info about the hash table in the file
-class fheader_page : public page {
-	private :
-		int original_size;
-		int n_buckets;
-		int n_pages;
-		int level;
-		int next;
+		public :
+			fheader_page() : page(page::fheader_page) {}
+			inline void set_orig_size(int);
+			inline void set_n_buckets(int);
+			inline void set_n_pages(int);
+			inline void set_level(int);
+			inline void set_next(int);
+			inline int get_orig_size();
+			inline int get_n_buckets();
+			inline int get_n_pages();
+			inline int get_level();
+			inline int get_next();
+	};
 
-	public :
-		fheader_page() : page(page::fheader_page) {}
-		inline void set_orig_size(int);
-		inline void set_n_buckets(int);
-		inline void set_n_pages(int);
-		inline void set_level(int);
-		inline void set_next(int);
-		inline int get_orig_size();
-		inline int get_n_buckets();
-		inline int get_n_pages();
-		inline int get_level();
-		inline int get_next();
-};
-
-inline void fheader_page::set_orig_size(int orig_size) {original_size = orig_size;}
-inline void fheader_page::set_n_buckets(int buckets) { n_buckets = buckets; }
-inline void fheader_page::set_n_pages(int pages) { n_pages = pages; }
-inline void fheader_page::set_level(int l) {level = l; }
-inline void fheader_page::set_next(int n) {next = n; }
-inline void fheader_page::ger_orig_size() {return original_size;}
-inline int fheader_page::get_n_buckets() {return n_buckets;}
-inline int fheader_page::get_n_pages() {return n_pages;}
-inline int fheader_page::get_level() {return level;}
-inline int fheader_page::get_next() {return next;}
-//----------end fheader_page class----------
+	inline void fheader_page::set_orig_size(int orig_size) {original_size = orig_size;}
+	inline void fheader_page::set_n_buckets(int buckets) { n_buckets = buckets; }
+	inline void fheader_page::set_n_pages(int pages) { n_pages = pages; }
+	inline void fheader_page::set_level(int l) {level = l; }
+	inline void fheader_page::set_next(int n) {next = n; }
+	inline void fheader_page::ger_orig_size() {return original_size;}
+	inline int fheader_page::get_n_buckets() {return n_buckets;}
+	inline int fheader_page::get_n_pages() {return n_pages;}
+	inline int fheader_page::get_level() {return level;}
+	inline int fheader_page::get_next() {return next;}
+	//----------end fheader_page class----------
 
 
-//pointer page. Stores pointers to overflow pages from the buckets
-class pointer_page : public page {
-	public :
-		static const int n_pointers = 28;
-		pointer_page() : page(page::pointer_page) {}
-		pointer_page(std::array<int,n_pointers> ptrs, int bitmap) : page(page::pointer_page) , ptr_bitmap(bitmap), ptr_array(ptrs) {}
-		inline void add_ptr(int);
-		inline void remove_ptr(int);
-		inline int get_ptr(int);
-		inline bool is_full();
-	private :
-		std::bitset<n_pointers>   ptr_bitmap;
-		std::array<int,n_pointers> ptr_array;
-		inline int available_slot();
-};
+	//pointer page. Stores pointers to overflow pages from the buckets
+	class pointer_page : public page {
+		public :
+			static const int n_pointers = 28;
+			pointer_page() : page(page::pointer_page) {}
+			pointer_page(std::array<int,n_pointers> ptrs, int bitmap) : page(page::pointer_page) , ptr_bitmap(bitmap), ptr_array(ptrs) {}
+			inline void add_ptr(int);
+			inline void remove_ptr(int);
+			inline int get_ptr(int);
+			inline bool is_full();
+		private :
+			std::bitset<n_pointers>   ptr_bitmap;
+			std::array<int,n_pointers> ptr_array;
+			inline int available_slot();
+	};
 
-//get leftmost available slot
-inline int pointer_page::available_slot() {
-	auto bmp = ptr_bitmap.to_ulong(); int i=0;
-	while (bmp%2 == 1) {
-		bmp = bmp >> 1;
-		++i;
+	//get leftmost available slot
+	inline int pointer_page::available_slot() {
+		auto bmp = ptr_bitmap.to_ulong(); int i=0;
+		while (bmp%2 == 1) {
+			bmp = bmp >> 1;
+			++i;
+		}
+		return i;
 	}
-	return i;
-}
 
-//insert pointer in available slot
-inline void pointer_page::add_ptr(int ptr) {
-	if (!ptr_bitmap.all()) {
-		int i = available_slot();
-		ptr_array[i] = ptr;
-		ptr_bitmap.set(i);
+	//insert pointer in available slot
+	inline void pointer_page::add_ptr(int ptr) {
+		if (!ptr_bitmap.all()) {
+			int i = available_slot();
+			ptr_array[i] = ptr;
+			ptr_bitmap.set(i);
+		}
 	}
-}
 
-inline void pointer_page::remove_ptr(int pos) {
-	if (pos < n_pointers) ptr_bitmap.reset(pos);
-}
-
-inline int pointer_page::get_ptr (int i) { return ptr_array[i]; }
-inline bool pointer_page::is_full() { return ptr_bitmap.all(); }
-//----------end pointer_page class----------
-
-//content page. Stores the actual index data, i.e. a lot of <key,rid>'s
-class content_page : public page {
-	private :
-		std::bitset<n_data>   data_bitmap;
-		std::array<int,n_data> data_array;
-		inline int available_slot();
-	public :
-		static const int n_data = 14;
-		content_page() : page(page::content_page) {}
-		inline void add_data(std::pair<int,int>);
-		inline std::pair <int,int> get_data();
-		inline bool is_full();
-
-};
-
-inline int content_page::available_slot(){
-	auto bmp = data_bitmap.to_ulong();
-	int i = 0;
-	while(bmp%2 == 1){
-		bmp = bmp >> 1;
-		++i;
+	inline void pointer_page::remove_ptr(int pos) {
+		if (pos < n_pointers) ptr_bitmap.reset(pos);
 	}
-	return i;
-}
 
-inline void content_page::add_data(std::pair<int,int> p){
-	if(!data_bitmap.all()){
-		int i = available_slot();
-		data_array[i] = p;
-		data_bitmap.set(i);
+	inline int pointer_page::get_ptr (int i) { return ptr_array[i]; }
+	inline bool pointer_page::is_full() { return ptr_bitmap.all(); }
+	//----------end pointer_page class----------
+
+	//content page. Stores the actual index data, i.e. a lot of <key,rid>'s
+	class content_page : public page {
+		private :
+			std::bitset<n_data>   data_bitmap;
+			std::array<int,n_data> data_array;
+			inline int available_slot();
+		public :
+			static const int n_data = 14;
+			content_page() : page(page::content_page) {}
+			inline void add_data(std::pair<int,int>);
+			inline std::pair <int,int> get_data();
+			inline bool is_full();
+
+	};
+
+	inline int content_page::available_slot(){
+		auto bmp = data_bitmap.to_ulong();
+		int i = 0;
+		while(bmp%2 == 1){
+			bmp = bmp >> 1;
+			++i;
+		}
+		return i;
 	}
-}
 
-inline std:pair<int,int> content_page::get_data(int i) { return data_array[i]; }
-inline bool content_page::is_full() { return data_bitmap.all(); }
-//----------end content_page class----------
+	inline void content_page::add_data(std::pair<int,int> p){
+		if(!data_bitmap.all()){
+			int i = available_slot();
+			data_array[i] = p;
+			data_bitmap.set(i);
+		}
+	}
+
+	inline std:pair<int,int> content_page::get_data(int i) { return data_array[i]; }
+	inline bool content_page::is_full() { return data_bitmap.all(); }
+	//----------end content_page class----------
+}
